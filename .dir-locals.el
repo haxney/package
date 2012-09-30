00d713163bfb9ca4f5f08c75d56c498301241b6a
Daniel Hackney <dan@haxney.org>
2012-09-30 13:05:43 -0400

    Get rid of `indent-tabs-mode'
    I never liked it anyway.

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((nil
-  (sentence-end-double-space . t)
-  (fill-column . 70))
- (emacs-lisp-mode
-  (indent-tabs-mode . t)))
